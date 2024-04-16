ca0fd57d259fd890b3520b8ed463485ec7202686
Luke Ward <lbw4459@rit.edu>
2024-04-15 20:56:02 -0400

    finished sapphire basics, need to get emerald textures but so far so good

--- src/main/java/com/luke/funmod/datagen/ModItemTagGenerator.java
+++ src/main/java/com/luke/funmod/datagen/ModItemTagGenerator.java
@@ -1,9 +1,11 @@
 package com.luke.funmod.datagen;
 
 import com.luke.funmod.funmod;
+import com.luke.funmod.item.ModItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
@@ -17,6 +19,16 @@ public class ModItemTagGenerator extends ItemTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.Provider pProvider) {
+        this.tag(ItemTags.TRIMMABLE_ARMOR)
+                .add(ModItems.SAPPHIRE_HELMET.get(),
+                    ModItems.SAPPHIRE_CHESTPLATE.get(),
+                    ModItems.SAPPHIRE_LEGGINGS.get(),
+                    ModItems.SAPPHIRE_BOOTS.get());
 
+        this.tag(ItemTags.TRIMMABLE_ARMOR)
+                .add(ModItems.EMERALD_HELMET.get(),
+                        ModItems.EMERALD_CHESTPLATE.get(),
+                        ModItems.EMERALD_LEGGINGS.get(),
+                        ModItems.EMERALD_BOOTS.get());
     }
 }
